c98da4c88e5b4e8bd20dbaa662becf17e9412dae
ShreyasJenner <shreyasjenner4@gmail.com>
2024-03-13 17:13:16 +0530

    basic gui

--- gui.py
+++ gui.py
@@ -1,21 +1,47 @@
 import streamlit as st
+import subprocess
+import os
 from PIL import Image
 
 def main():
-    st.title("Dual Image Viewer")
+    st.title("NFA to DFA Converter")
 
-    if True:
-        # Read the image files
-        image1 = "images/nfa.png" 
-        image2 = "images/dfa.png" 
+    # Input parameters for the nfa python file 
+    param1 = st.file_uploader("Input File:", type=["json"])
+    param2 = st.text_input("Output File Name:")
 
-        # Display the images side by side
-        col1, col2 = st.columns(2)
-        with col1:
-            st.image(image1, caption="Image 1", use_column_width=True)
+    # Button to execute the external script
+    if st.button("Convert"):
+        execute_script(param1.name, param2)
+        image()
 
-        with col2:
-            st.image(image2, caption="Image 2", use_column_width=True)
+def execute_script(param1, param2):
+    try:
+        script_path = "main.py"
+
+        # Run the nfa converted with venv 
+        command = ["venv/bin/python", script_path, param1, param2]
+
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, text=True)
+
+        stdout, stderr = process.communicate(input=f"{param1}\n{param2}\n")
+    except Exception as e:
+        st.error(f"Error executing script: {e}")
+
+
+def image():
+    st.title("Image Viewer")
+
+    # store path for the image files
+    image1 = "dfa.png" 
+    image2 = "nfa.png" 
+
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image(image1, caption="NFA", use_column_width=True)
+
+    with col2:
+            st.image(image2, caption="DFA", use_column_width=True)
 
 if __name__ == "__main__":
     main()
